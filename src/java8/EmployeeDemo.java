58b0f02e37dcbd7194f06ba12956c5a2b9b7aea1
Nisha Singh <nishanamit1994@gmail.com[A[Aode>
2024-06-13 12:44:47 +0530

    second commit

---
+++ src/java8/EmployeeDemo.java
@@ -0,0 +1,28 @@
+package java8;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class EmployeeDemo {
+	
+	public static void main(String[] args) {
+		
+		//Employee emp = new Employee();
+		List<Employee> list = Arrays.asList();
+	
+		  new Employee(101,"Nisha","nishasingh39107@gmail.com","Pune","IT","Female",28);
+		  new Employee(102,"Amit","amitkumar11@gmail.com","Delhi","HR","male",29);
+		  new Employee(103,"Lata","latasingh23@gmail.com","Noida","Developer","Female",30);
+		  new Employee(104,"Nish","nishasingh39107","Pune","IT","Female",31);
+		  new Employee(105,"Nisha","nishasingh39107","Pune","IT","Female",32);
+		  new Employee(106,"Nisha","nishasingh39107","Pune","IT","Female",28);
+		  new Employee(107,"Nisha","nishasingh39107","Pune","IT","Female",28);
+		  new Employee(108,"Nisha","nishasingh39107","Pune","IT","Female",28);
+		
+	
+		  
+		 
+		
+	}
+
+}
