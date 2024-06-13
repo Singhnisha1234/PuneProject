0a92d5381def30b6a1d4269c6ef3451846952c37
Nisha Singh <nishanamit1994@gmail.com[A[Aode>
2024-06-13 13:51:04 +0530

    forth commit

--- src/java8/EmployeeDemo.java
+++ src/java8/EmployeeDemo.java
@@ -16,6 +16,7 @@ public class EmployeeDemo {
 		  new Employee(103,"Lata","latasingh23@gmail.com","Noida","Developer","Female",30);
 		  new Employee(104,"Isha","isha123@gmail.com","Delhi","Software Developer","Female",27);
 		  new Employee(105,"Naina","naina345@gmail.com","Mumbai","Developer","Female",26);
+		  new Employee(105,"Naina","naina345@gmail.com","Mumbai","Developer","Female",26);
 		
 	List<Employee> l = list.stream().filter(n -> n.getAge()>26).collect(Collectors.toList());
 		  System.out.println(l);
