91fbd01179fab7e257646b02fbd048215198cba1
Nisha Singh <nishanamit1994@gmail.com[A[Aode>
2024-06-13 13:03:06 +0530

    third commit

--- src/java8/EmployeeDemo.java
+++ src/java8/EmployeeDemo.java
@@ -2,6 +2,7 @@ package java8;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class EmployeeDemo {
 	
@@ -13,14 +14,11 @@ public class EmployeeDemo {
 		  new Employee(101,"Nisha","nishasingh39107@gmail.com","Pune","IT","Female",28);
 		  new Employee(102,"Amit","amitkumar11@gmail.com","Delhi","HR","male",29);
 		  new Employee(103,"Lata","latasingh23@gmail.com","Noida","Developer","Female",30);
-		  new Employee(104,"Nish","nishasingh39107","Pune","IT","Female",31);
-		  new Employee(105,"Nisha","nishasingh39107","Pune","IT","Female",32);
-		  new Employee(106,"Nisha","nishasingh39107","Pune","IT","Female",28);
-		  new Employee(107,"Nisha","nishasingh39107","Pune","IT","Female",28);
-		  new Employee(108,"Nisha","nishasingh39107","Pune","IT","Female",28);
+		  new Employee(104,"Isha","isha123@gmail.com","Delhi","Software Developer","Female",27);
+		  new Employee(105,"Naina","naina345@gmail.com","Mumbai","Developer","Female",26);
 		
-	
-		  
+	List<Employee> l = list.stream().filter(n -> n.getAge()>26).collect(Collectors.toList());
+		  System.out.println(l);
 		 
 		
 	}
